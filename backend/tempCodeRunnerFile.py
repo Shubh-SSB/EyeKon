4581296d974d1aa0531070e3a00c471eb1552486
Shubh-SSB <shubham.bhatnagar234@gmail.com>
2025-04-10 19:59:45 +0530

    UNDO

--- backend/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-@socketio.on('eye_fatigue_score')
-# def handle_eye_fatigue_score(data):
-#     efs = data.get('efs')
-#     if efs is not None:
-#         print(f"[Server] EFS received: {efs}")
-#         auto_adjust_screen(float(efs)) 
-#     emit('efs_ack', {'status': 'received'})
